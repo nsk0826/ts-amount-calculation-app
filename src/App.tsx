bbe733c2f900b90e983b3981c530113c7810ee03
nsk0826 <nsk.20160401@gmail.com>
2021-12-24 00:29:43 +0900

    初期環境作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class Detail extends React.Component {
+  render() {
+    return (
+      <div >
+        <div className="classification-name">名前</div>
+        <div className="description">説明</div>
+        <div className="unit-price">0円</div>
+        <div className="num-people">
+          <select value="0">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+          <span>名</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="party">
+          <input type="text" className="party" value="0" />
+          <span>名様</span>
+        </div>
+        <div className="total-amount">
+          <span>合計</span>
+          <input type="text" className="total-amount" value="0" />
+          <span>円</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class AdmissionFeeCalculator extends React.Component {
+  render() {
+    return (
+      <>
+        <Detail />
+        <Summary />
+      </>
+    );
+  }
+}
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <AdmissionFeeCalculator />
     </div>
   );
 }
